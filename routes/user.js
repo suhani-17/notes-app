7f45b57faeaf7cbff12e23daddea7229f186dbc6
Suhani Jain <suhani.j2002@gmail.com>
2024-07-15 22:48:06 +0530

    setting up login route

--- routes/user.js
+++ routes/user.js
@@ -22,3 +22,25 @@ router.post('/signup', async (req, res) => {
 
 
   });
+
+  router.post('/login', async (req, res) => {
+
+    try {
+      const { email, password } = req.body;
+
+      const user = await User.findOne({ email });
+
+      if (!user || !(await user.comparePassword(password))) {
+        return res.status(400).send({ error: 'Credentials are Invalid!' });
+      }
+  
+      const token = jwt.sign({ _id: user._id }, process.env.secret, { expiresIn: '1h' });
+
+      res.send({ token });
+    } 
+
+    catch (error) {
+      res.status(500).send({ error: error.message });
+    }
+    
+  });
